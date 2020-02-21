195d8c9a7b6455f604c2c120f6be8215de343f0e
Neilson Flemming <NeilsonFlemming@gmail.com>
2020-02-21 17:40:11 -0500

    Inital commit

---
+++ index.js
@@ -0,0 +1,54 @@
+// This is my first JS code
+console.log('Hello World');
+
+//setting variables
+//names should be meaningful
+//String Literal
+let firstName = 'Neilson';
+let lastName = 'Flemming';
+//Number Literal
+let age =29;
+//Boolean Literal
+let isApproved = true;
+//not assigned
+let selectedColor = null;
+
+//print
+console.log(firstName,lastName);
+console.log(age,isApproved)
+
+// when using const (constant) you cannot re-assign the value
+const interestRate = 1;
+console.log(interestRate);
+
+
+let person = {
+    firstName : 'Neilson',
+    lastName : 'Flemming',
+    age : 29
+};
+
+console.log(person);
+console.log(person.firstName);
+console.log(person['lastName']);
+
+
+let selectedColors = ['red','blue'];
+selectedColors[2] = 'green';
+
+console.log(selectedColors);
+console.log(selectedColors[0]);
+
+function myFunction(firstName,lastName) {
+    console.log('My First Function. My first name is ' + firstName);
+    console.log('My First Function. My Last name is ' + lastName);
+}
+
+myFunction('Neilson','Flemming');
+
+function square(number) {
+    squareRoot = number * number;
+    return squareRoot
+}
+
+console.log(square(16))
